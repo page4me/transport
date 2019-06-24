bde45d46a41fa3108e59865226609cce80239b58
page4me <info@page4me.pl>
2019-06-24 20:23:46 +0200

    update migracji wykaz_poj

--- database/migrations/2019_06_24_175607_create_wykaz_poj_table.php
+++ database/migrations/2019_06_24_175607_create_wykaz_poj_table.php
@@ -15,7 +15,20 @@ class CreateWykazPojTable extends Migration
     {
         Schema::create('wykaz_poj', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->bigInteger('id_przed')->nullable();
+            $table->bigInteger('id_dok_przed')->nullable();
+            $table->string('rodzaj_poj')->nullable();
+            $table->string('marka')->nullable();
+            $table->string('nr_rej')->nullable();
+            $table->string('nr_vin')->nullable();
+            $table->string('dmc')->nullable();
+            $table->string('wlasnosc')->nullable();
+            $table->date('data_wpr')->nullable();
+            $table->date('data_wyc')->nullable();
+            $table->integer('status', 2);
+            $table->date('stan')->nullable();
+            $table->timestamps('create_at');
+            $table->timestamps('create_up');
         });
     }
 
