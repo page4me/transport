4d7cb7a1c7e722c8361cbd8beac962171f85889f
page4me <info@page4me.pl>
2019-06-24 20:01:40 +0200

    utworzenie migracji decyzje

---
+++ database/migrations/2019_06_24_175926_create_decyzje_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDecyzjeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('decyzje', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('decyzje');
+    }
+}
