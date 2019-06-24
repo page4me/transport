4b4e34bc65f55985f3f5ff3b783ded542e21d941
page4me <info@page4me.pl>
2019-06-24 20:16:22 +0200

    utworzenie mirgracji dok_przed_wyp

---
+++ database/migrations/2019_06_24_181418_create_dok_przed_wyp_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDokPrzedWypTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dok_przed_wyp', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('id_dok_przed')->nullable();
+            $table->bigInteger('id_przed')->nullable();
+            $table->string('nazwa')->nullable();
+            $table->string('nr_wyp')->nullable();
+            $table->string('nr_druku')->nullable();
+            $table->string('nr_sprawy')->nullable();
+            $table->date('data_wn')->nullable();
+            $table->date('data_wyd')->nullable();
+            $table->timestamps('create_at');
+            $table->timestamps('create_up');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dok_przed_wyp');
+    }
+}
