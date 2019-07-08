3cf7c6609ba8f49ff352bba7e1c51b0278cc0b9b
page4me <info@page4me.pl>
2019-07-08 21:35:40 +0200

    uzupenienie modelu DokumentyPrzed

--- app/DokumentyPrzed.php
+++ app/DokumentyPrzed.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class DokumentyPrzed extends Model
 {
-    //
+    public $table = 'dok_przed';
+    protected $fillable = ['id_przed','nazwa', 'rodz_dok', 'nr_dok', 'nr_druku', 'nr_sprawy', 'data_wn', 'data_wyd', 'dat_umowy', 'data_waz', 'uwagi'];
+
 }
