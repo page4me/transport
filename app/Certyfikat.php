84b8bc202c02b45fd0fd4409d555875045137706
page4me <info@page4me.pl>
2019-07-19 19:38:31 +0200

    uzupelnienie modeku Certyfikat

--- app/Certyfikat.php
+++ app/Certyfikat.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Certyfikat extends Model
 {
     //
+    public $table = 'cert_komp';
+    protected $fillable = ['id_przed','rodzaj','nr_cert', 'imie_os_z','nazwisko_os_z','adres', 'miasto', 'dat_ur', 'dat_wyd', 'os_zarz','umowa','dat_umowy', 'uwagi'];
 }
