b1bda7f6c4d2927f440619c0db512b9f16a48bd8
page4me <info@page4me.pl>
2019-07-19 19:41:59 +0200

    uzupelnienie kontrolera Zdolnosc

--- app/Http/Controllers/ZdolnoscController.php
+++ app/Http/Controllers/ZdolnoscController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use Alert;
 
 class ZdolnoscController extends Controller
 {
@@ -24,6 +26,9 @@ class ZdolnoscController extends Controller
     public function create()
     {
         //
+          $przedsiebiorca= DB::table('przedsiebiorca')->get();
+
+        return view('przedsiebiorca.zabezpieczenie.create', compact('przedsiebiorca'));
     }
 
     /**
@@ -35,6 +40,22 @@ class ZdolnoscController extends Controller
     public function store(Request $request)
     {
         //
+
+        Alert::success('Dodano nowe zabezpieczenie', 'Zdolnosć finansowa przypisana do przedsiębiorcy');
+        $validatedData = $request->validate([
+         'id_przed' => 'required|max:1',
+         'nazwa' => 'string|max:255|nullable',
+         'numer' => 'string|max:255|nullable',
+         'data_od' => 'string|max:255|nullable',
+         'data_do' => 'string|max:255|nullable',
+         'ile_poj' => 'required|max:255',
+         'suma_zab' => 'required|max:255',
+         'status' => 'required|max:16|nullable',
+         'uwagi' => 'string|max:255|nullable'
+        ]);
+        $baza = \App\Zdolnosc::create($validatedData);
+
+        return redirect('/przedsiebiorca')->with('success', 'Baza eksploatacyjna przypisana do przedsiębiorcy');
     }
 
     /**
