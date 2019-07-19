18b3fc02b321b8a8f49cd9f57930032deecc8c4d
page4me <info@page4me.pl>
2019-07-19 19:41:46 +0200

    uzupelnienie modelu Zdolnosc

--- app/Zdolnosc.php
+++ app/Zdolnosc.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Zdolnosc extends Model
 {
     //
+    public $table = 'zdol_finans';
+    protected $fillable = ['id_przed','nazwa', 'numer', 'data_od','data_do','ile_poj', 'suma_zab', 'status', 'uwagi'];
 }
