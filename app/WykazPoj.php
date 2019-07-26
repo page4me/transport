ccc12d44ee833c2a45fa93aa36476847246819f8
page4me <info@page4me.pl>
2019-07-26 19:18:22 +0200

    aktualizacja Modelu WykazPoj

--- app/WykazPoj.php
+++ app/WykazPoj.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class WykazPoj extends Model
 {
     //
+    public $table = 'wykaz_poj';
+    protected $fillable = ['id_przed','id_dok_przed', 'rodzaj_poj', 'marka', 'nr_rej','p_nr_rej', 'nr_vin', 'dmc', 'wlasnosc', 'data_wpr', 'data_wyc', 'status', 'stan', 'uwagi'];
 }
