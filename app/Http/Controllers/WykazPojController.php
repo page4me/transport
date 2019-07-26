470966f582cd721084344d1169419d2f13d04a0c
page4me <info@page4me.pl>
2019-07-26 19:18:59 +0200

    korekta kontrolera WykazPoj

--- app/Http/Controllers/WykazPojController.php
+++ app/Http/Controllers/WykazPojController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use Alert;
 
 class WykazPojController extends Controller
 {
@@ -14,6 +16,11 @@ class WykazPojController extends Controller
     public function index()
     {
         //
+        //$przedsiebiorca = \App\Przedsiebiorca::all();
+
+       // $cars = DB::table('wykaz_poj')->where('id', $przedsiebiorca->id)->get();
+
+       // return view('przedsiebiorca.show', compact('przedsiebiorca','cars'));
         
     }
 
@@ -25,6 +32,9 @@ class WykazPojController extends Controller
     public function create()
     {
         //
+        $przedsiebiorca= DB::table('wykaz_poj')->get();
+         
+        return view('przedsiebiorca.cars', compact('przedsiebiorca'));
     }
 
     /**
@@ -36,6 +46,26 @@ class WykazPojController extends Controller
     public function store(Request $request)
     {
         //
+        // $przedsiebiorca = \App\Przedsiebiorca::findOrFail($id);
+       // Alert::success('Dodano nowy pojazd', '');
+        $validatedData = $request->validate([
+         'id_przed' => 'required',
+         'id_dok_przed' => 'required',
+         'rodzaj_poj' => 'required|max:255',
+         'marka' => 'required|max:255',
+         'nr_rej' => 'required|max:255',
+         'nr_vin' => 'required|max:255',
+         'dmc' => 'required|max:255',
+         'wlasnosc' => 'required|max:255',
+         'data_wpr' => 'required|max:11',
+         'status' => 'default|1',
+                  
+        ]);
+        $pojazdy = \App\WykazPoj::create($validatedData);
+       
+       // $dok = DB::table('wykaz_poj')->where('id_przed' , $przed->id)->get();
+
+        return redirect('przedsiebiorca/cars/'.$request->id_przed)->with('success', 'Przedsiebiorca dodany do bazy danych');
     }
 
     /**
@@ -57,7 +87,11 @@ class WykazPojController extends Controller
      */
     public function edit($id)
     {
-        //
+         $cars = \App\WykazPoj::findOrFail($id);
+         //$dok = DB::table('dok_przed')->where('id_przed')->get();
+         //echo '<pre/>';
+         //print_r($cars);
+         return view('przedsiebiorca.pojazdy.edit', compact('cars'));
     }
 
     /**
@@ -70,6 +104,22 @@ class WykazPojController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $validatedData = $request->validate([
+         'id_przed' => 'required',
+         'id_dok_przed' => 'required',
+         'rodzaj_poj' => 'required|max:255',
+         'marka' => 'required|max:255',
+         'nr_rej' => 'required|max:255',
+         'nr_vin' => 'required|max:255',
+         'dmc' => 'required|max:255',
+         'wlasnosc' => 'required|max:255',
+         'data_wpr' => 'required|max:11',
+         'status' => 'default|1',
+                  
+        ]);
+        \App\WykazPoj::whereId($id)->update($validatedData);
+
+        return redirect('/przedsiebiorca/cars/$id')->with('success', 'Dane przedsiębiorcy zmienione');
     }
 
     /**
