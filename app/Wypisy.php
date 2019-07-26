6aec810637b812dedff7b37d850e34b373ec1720
page4me <info@page4me.pl>
2019-07-26 19:18:42 +0200

    uzupelnienie Modelu Wypisy

--- app/Wypisy.php
+++ app/Wypisy.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Wypisy extends Model
 {
     //
+    public $table = 'dok_przed_wyp';
+    protected $fillable = ['id_dok_przed','id_przed','nazwa', 'rodzaj_wyp', 'nr_wyp', 'nr_druku','nr_sprawy', 'data_wn', 'data_wyd', 'uwagi'];
 }
