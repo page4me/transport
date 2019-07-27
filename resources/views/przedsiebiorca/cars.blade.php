3b6b59d32f293fac75e1737b1f1bdf5dadffdccc
page4me <info@page4me.pl>
2019-07-28 00:15:16 +0200

    aktulizacja dziala modal ale przy update zmienia dwa rekordy !

--- resources/views/przedsiebiorca/cars.blade.php
+++ resources/views/przedsiebiorca/cars.blade.php
@@ -172,8 +172,11 @@
                    <td class="text-center">{{$car->wlasnosc}}</td>
                    <td class="text-center">wprowadzono <br />{{$car->data_wpr}} r.</td>
                    <td class="text-center" colspan="2">
-                    <a href="/przedsiebiorca/pojazdy/{{$car->id}}" data-toggle="modal" data-id="{{$car->id}}" data-target="#editModal" role="button" class="btn btn-success btn-sm" alt="Edycja" ><i class="fa fa-edit"></i></a>
+                   
+                    <a href="/przedsiebiorca/pojazdy/{{$car->id}}" data-toggle="modal" data-id="{{$car->id}}" data-nr_rej="{{$car->nr_rej}}"  data-marka="{{$car->marka}}" data-nr_vin="{{$car->nr_vin}}" data-wlasnosc="{{$car->wlasnosc}}" data-data_wpr="{{$car->data_wpr}}" data-dmc="{{$car->dmc}}" data-rodzaj_poj="{{$car->rodzaj_poj}}" data-target="#editModal" role="button" class="btn btn-success btn-sm" alt="Edycja" ><i class="fa fa-edit"></i></a>
+
                     <a href="#" role="button" class="btn btn-danger btn-sm">Wycofaj</a>
+
                                       <!-- edit modal -->
                                 <!-- Modal -->
                                   <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -195,16 +198,16 @@
                                           </button>
                                         </div>
                                         <div class="modal-body">
-                                          {{print_r($cid)}}
-                                              {{$car->id}}
+                                          
+                                             
                                             <!-- Form add new car -->
-                                                <form method="post" action="{{ route('pojazdy.update', 'id' ) }}">
+                                                <form method="post" action="{{ route('pojazdy.update', $car->id ) }}">
                                               <div class="row">
                                                     @csrf
-                                                     @method('PATH')
+                                                      @method('PATCH')
                                                  <div class="col-md-12 form-group">
                                                     <label for="nr_rej"><strong>Numer rejestracyjny:</strong></label>
-                                                    <input type="text" class="form-control" name="nr_rej" value="{{$car->nr_rej}}"/>
+                                                    <input type="text" class="form-control" name="nr_rej" id="nr_rej" value="{{$car->nr_rej}}"/>
                                                 </div>
                                                </div>
 
@@ -212,15 +215,15 @@
 
                                                 <div class="col-md-4 form-group">
                                                     <label for="nr_dok"><strong>Rodzaj pojazdu:</strong></label>
-                                                    <input type="text" class="form-control" name="rodzaj_poj" value="{{$car->rodzaj_poj}}" />
+                                                    <input type="text" class="form-control" name="rodzaj_poj" id="rodzaj_poj" value="{{$car->rodzaj_poj}}" />
                                                 </div>
                                                 <div class="col-md-4 form-group">
                                                     <label for="kod_p"><strong>Marka:</strong></label>
-                                                    <input type="text" class="form-control" name="marka" value="{{$car->marka}}" />
+                                                    <input type="text" class="form-control" name="marka" id="marka" value="{{$car->marka}}" />
                                                 </div>
                                                  <div class="col-md-4 form-group">
                                                     <label for="nr_druku"><strong>Nr VIN:</strong></label>
-                                                    <input type="text" class="form-control" name="nr_vin" maxlength="17" value="{{$car->nr_vin}}" />
+                                                    <input type="text" class="form-control" name="nr_vin" id="nr_vin" maxlength="17" value="{{$car->nr_vin}}" />
                                                 </div>
 
                                               </div>
@@ -229,26 +232,27 @@
                                               <div class="row">
                                                  <div class="col-md-4 form-group">
                                                     <label for="nr_sprawy"><strong>DMC:</strong></label>
-                                                    <input type="text" class="form-control" name="dmc" value="{{$car->dmc}}" />
+                                                    <input type="text" class="form-control" name="dmc" id="dmc" value="{{$car->dmc}}" />
                                                 </div>
                                                 <div class="col-md-4 form-group">
                                                     <label for="tel"><strong>Własnosć:</strong></label>
-                                                    <input type="text" class="form-control" name="wlasnosc" value="{{$car->wlasnosc}}" />
+                                                    <input type="text" class="form-control" name="wlasnosc" id="wlasnosc" value="{{$car->wlasnosc}}" />
                                                 </div>
                                                 <div class="col-md-4 form-group">
                                                     <label for="tel"><strong>Data wprowadzenia:</strong></label>
-                                                    <input type="date" class="form-control" name="data_wpr" value="{{$car->data_wpr}}" />
+                                                    <input type="date" class="form-control" name="data_wpr" id="data_wpr" value="{{$car->data_wpr}}" />
                                                 </div>
 
                                               </div>
                                               <div class="row">
                                                 <div class="col-md-12 form-group">
                                                     <label for="uwagi"><strong>Uwagi:</strong></label>
-                                                    <input type="text" class="form-control" name="uwagi" value="{{$car->uwagi}}" />
+                                                    <input type="text" class="form-control" name="uwagi" id="uwagi" value="{{$car->uwagi}}" />
                                                 </div>
                                               </div>
                                               <input type="hidden" name="id_przed" value="{{$dk->id_przed}}" />
                                               <input type="hidden" name="id_dok_przed" value="{{$dk->id}}" />
+                                              <input type="hidden" name="id" id="id" value="{{$car->id}}" />
                                         </div>
                                         <div class="modal-footer">
 
@@ -260,7 +264,33 @@
                                         </div>
                                       </div>
                                     </div>
-                                  
+                                  <script type="text/javascript">
+                                     $('#editModal').on('show.bs.modal', function (event) {
+                                              var button = $(event.relatedTarget) 
+                                              var marka = button.data('marka') 
+                                              var nr_rej = button.data('nr_rej') 
+                                              var nr_vin = button.data('nr_vin') 
+                                              var data_wpr = button.data('data_wpr')
+                                              var rodzaj_poj = button.data('rodzaj_poj')
+                                              var wlasnosc = button.data('wlasnosc')
+                                              var dmc = button.data('dmc')
+                                              var uwagi = button.data('uwagi') 
+                                              
+                                              
+                                              var modal = $(this)
+                                              modal.find('.modal-body #marka').val(marka);
+                                              modal.find('.modal-body #nr_rej').val(nr_rej);
+                                              modal.find('.modal-body #nr_vin').val(nr_vin);
+                                              modal.find('.modal-body #data_wpr').val(data_wpr);
+                                              modal.find('.modal-body #rodzaj_poj').val(rodzaj_poj);
+                                              modal.find('.modal-body #dmc').val(dmc);
+                                              modal.find('.modal-body #wlasnosc').val(wlasnosc);
+                                              modal.find('.modal-body #uwagi').val(uwagi);
+                                             
+                                             
+                                             
+                                        })
+                                  </script>
 
                                
                                
