305dac69c3a3223e142b20b3ec571977ec9df3ec
ermo <merys@o2.pl>
2019-07-27 22:59:43 +0200

    zmiana znaku

--- resources/views/przedsiebiorca/index.blade.php
+++ resources/views/przedsiebiorca/index.blade.php
@@ -19,7 +19,7 @@
           <td>Imię</td>
           <td>Nazwisko</td>
           <td>Adres</td>
-          <td>Miejscowosć</td>
+          <td>Miejscowość</td>
           <td>NIP</td>
           <td>REGON</td>
           <td>Telefon<td>
