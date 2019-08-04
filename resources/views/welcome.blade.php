9f28434971c0f9660fad021d611006f7d2b4c8d5
page4me <info@page4me.pl>
2019-08-04 17:34:34 +0200

    zmiana tytulu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>T.O.S ver. 1.0</title>
   <title>Hi</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <meta name="csrf-token" content="{{ csrf_token() }}">
