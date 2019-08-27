b923376b787d48296dbe60884d0b7b5b15b62417
page4me <info@page4me.pl>
2019-08-27 08:55:23 +0200

    dodanie select wlasnosc

--- resources/views/przedsiebiorca/cars.blade.php
+++ resources/views/przedsiebiorca/cars.blade.php
@@ -112,6 +112,12 @@
                         <div class="col-md-4 form-group">
                             <label for="tel"><strong>Własność:</strong></label>
                             <input type="text" class="form-control" name="wlasnosc" />
+                            <select name="wlasnosc" class="form-control">
+                              <option value="własność">własność</option>
+                              <option value="leasing">leasing</option>
+                              <option value="najem">najem</option>
+                              <option value="użyczenie">użyczenie</option>
+                            </select>
                         </div>
                         <div class="col-md-4 form-group">
                             <label for="tel"><strong>Data wprowadzenia:</strong></label>
