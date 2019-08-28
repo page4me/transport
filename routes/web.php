52bfbc6a74e2603e354f66166f36c237ddda6561
page4me <info@page4me.pl>
2019-08-28 21:37:35 +0200

    nowy routeing

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/przedsiebiorca/wypisy/{id}', 'WypisyController@index')->name('index
 Route::get('/przedsiebiorca/pdf/{id}', 'PrzedsiebiorcaController@gPDF');
 Route::get('/przedsiebiorca/wypisyPDF/{id}', 'WypisyController@wypisyPDF');
 Route::get('/przedsiebiorca/zabezpieczenie/stare', 'PrzedsiebiorcaController@stare_zf')->name('stare_zf');
-Route::get('/przedsiebiorca/pisma/print_zdol_finans', 'PrzedsiebiorcaController@print_zdol_finans')->name('print_zdol_finans');
+Route::get('/przedsiebiorca/pisma/print_zdol_finans/{id}', 'PrzedsiebiorcaController@print_zdol_finans');
 
 
 Route::resource('przedsiebiorca/baza', 'BazaController');
