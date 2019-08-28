6ca9ac0103b6f3599e610e4cb85056f84993658b
page4me <info@page4me.pl>
2019-08-28 14:22:28 +0200

    update

--- resources/views/przedsiebiorca/pisma/print.zdol_finans.blade.php
+++ resources/views/przedsiebiorca/pisma/print.zdol_finans.blade.php
@@ -1,1 +1,80 @@
-print.zdol_finans.blade.php
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Wykaz pojazdów</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <style>
+            /** Define the margins of your page **/
+            body {
+          font-family: DejaVu Sans;
+          font-size: 12px;
+          margin: 10px 10px 0 0;
+      }
+            @page {
+               margin: 10px 10px 0 0;
+
+            }
+
+        </style>
+</head>
+<body>
+  <div class="container">
+  	 <div>
+  	 	Starosta Koszaliński na podstawie  art. 83 ust. 1 ustawy z dnia 6 września 2001 r. o transporcie drogowym (Dz. U. z 2019 r., poz. 58 z późn. zm.) oraz § 2 ust. 3 rozporządzenia Ministra Infrastruktury i Rozwoju z dnia 8 września 2014 roku w sprawie danych i informacji, które przewoźnik drogowy jest obowiązany przekazywać do organu w związku z prowadzoną działalnością w zakresie przewozu drogowego (Dz.U. z 2014 r., poz.1217) nakłada na Pana obowiązek  przedłożenia w terminie 21 dni od dnia otrzymania tego pisma, dokumentów potwierdzających spełnienie wymagań ustawowych do otrzymanego „zezwolenia na wykonywanie zawodu przewoźnika drogowego” wyrażonego  licencją:
+  	 </div>
+  	 <div>
+  	 	nr 0139330 udzieloną dnia 1 czerwca 2016 roku na wykonywanie krajowego transportu drogowego rzeczy. tj.:
+  	 </div>
+  	 <div>
+  	  <ul>
+  	   <li>	
+		1) dokumenty potwierdzające spełnienie warunków, o których mowa w art. 7 rozporządzenia (WE) nr 1071/2009 związane z wymogiem zdolności finansowej. Przedsiębiorca musi być w stanie w każdym momencie roku finansowego spełnić swoje zobowiązania finansowe. W tym celu przedsiębiorca wykazuje na podstawie poświadczonych przez audytora lub odpowiednio upoważnioną osobę rocznych sprawozdań finansowych, że co roku dysponuje kapitałem i rezerwami o wartości co  najmniej równej 9 000 EUR w przypadku wykorzystywania tylko jednego pojazdu i 5 000 EUR na każdy dodatkowy wykorzystywany pojazd.
+       </li>
+       <li>
+		2) wykaz pojazdów zawierający następujące informacje: 
+		5) markę, typ; 2) rodzaj/przeznaczenie;  3) numer rejestracyjny;  4) numer
+		      VIN; wskazanie rodzaju tytułu prawnego do dysponowania pojazdem wraz
+		      z kserokopiami dowodów rejestracyjnych, z aktualnymi badaniami
+		      technicznymi;
+	   </li>
+	  </ul>
+  	 </div>
+  	 <div>
+  	 	W drodze odstępstwa właściwy organ może zgodzić się lub wymagać, aby przedsiębiorca wykazał swoją zdolność finansową za pomocą zabezpieczenia, takiego jak gwarancja bankowa lub ubezpieczenie, w tym ubezpieczenie odpowiedzialności zawodowej z jednego lub kilku banków lub innych instytucji finansowych, w tym przedsiębiorstw ubezpieczeniowych, składających solidarną gwarancję za przedsiębiorstwo na kwoty określone przy posiadaniu pojazdów samochodowych przeznaczonych do transportu drogowego.
+  	 </div>
+  	 <div>Pouczenie</div>
+  	 <div>
+  	 	Jednocześnie informuję, że jeśli organ udzielający „zezwolenia na wykonywanie zawodu przewoźnika drogowego” wyrażonego licencją na wykonywanie krajowego transportu drogowego rzeczy, stwierdzi  niespełnienie przez przedsiębiorcę wymogów będących podstawą do wydania tego dokumentu, organ ten zawiesza lub cofa zezwolenie (licencję) na wykonywanie zawodu przewoźnika drogowego.
+  	 </div>
+  </div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
