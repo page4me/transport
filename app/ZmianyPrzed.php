95ed18c1342d50d9a2ae36ae8a26d9cdab19fe74
page4me <info@page4me.pl>
2019-09-10 22:03:12 +0200

    update model historii zmian

--- app/ZmianyPrzed.php
+++ app/ZmianyPrzed.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class ZmianyPrzed extends Model
 {
     //
+    public $table = 'hist_zmian_przed';
+    protected $fillable = ['id_przed','id_dok_przed', 'nazwa_zm', 'data_zm'];
 }
