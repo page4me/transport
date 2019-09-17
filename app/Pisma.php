f6d1a8ae3eca511829aa4c71e1e8e16b67335845
page4me <info@page4me.pl>
2019-09-17 17:52:54 +0200

    update

--- app/Pisma.php
+++ app/Pisma.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Pisma extends Model
 {
     //
+    public $table = 'pisma';
+    protected $fillable = ['nazwa', 'tresc', 'nr_sprawy', 'data_p', 'id_przed'];
+
 }
