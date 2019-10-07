baa79e43542944ab29427004a1846ee18b6ae778
page4me <info@page4me.pl>
2019-10-07 21:16:38 +0200

    update

--- app/DokumentyPrzed.php
+++ app/DokumentyPrzed.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class DokumentyPrzed extends Model
 {
     public $table = 'dok_przed';
-    protected $fillable = ['id_przed','nazwa', 'rodz_dok', 'nr_dok', 'p_nr_dok','nr_druku', 'nr_sprawy', 'data_wn', 'data_wyd', 'dat_umowy', 'data_waz', 'uwagi'];
+    protected $fillable = ['id_przed','nazwa', 'rodz_dok', 'nr_dok', 'p_nr_dok','nr_druku', 'nr_sprawy', 'data_wn', 'data_wyd', 'dat_umowy', 'data_waz', 'powod', 'dat_zaw', 'dat_rez', 'status','uwagi'];
 
 }
