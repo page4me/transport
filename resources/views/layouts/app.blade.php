27c4b20a8f84e3fb5997737e2431109525c86013
page4me <info@page4me.pl>
2019-10-09 21:31:03 +0200

    add font awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,10 @@
        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <script src="https://kit.fontawesome.com/24f75c5d02.js"></script>
+        <script src="{{asset("/js/fontawesome.js")}}"></script>
+        <link href="{{asset("/css/fontawesome.css")}}" rel="stylesheet">
+        <script src="{{asset("/js/all.js")}}"></script>
+        <link href="{{asset("/css/all.css")}}" rel="stylesheet">
         <script src="{{asset("/vendor/tinymce/tinymce.min.js")}}"></script>
         <script src="https://lipis.github.io/bootstrap-sweetalert/dist/sweetalert.js"></script>
         <link rel="stylesheet" href="https://lipis.github.io/bootstrap-sweetalert/dist/sweetalert.css" />
